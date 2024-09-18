5dc62f362e03f3ca3dfb391926ba819f33a7f044
Alok0601 <alokr492@gmail.com>
2024-09-18 13:24:41 +0530

    Initial Commit

---
+++ School/SchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolApp.swift
+//  School
+//
+//  Created by Alok Ranjan on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
